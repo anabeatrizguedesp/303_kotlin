c0b9c61456c454cf23873383bc4f383e6dc3e271
u22524 <anabeatrizguedesp@gmail.com>
2023-04-03 20:29:47 -0300

    aula 03/04-23

---
+++ listaBase/app/src/main/java/com/example/listabase/lista1item.kt
@@ -0,0 +1,11 @@
+package com.example.listabase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class lista1item : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista1item)
+    }
+}
